13baaf69cb7d9030f66b793d799325c3408b2d4d
Rohit Nisal <rohit11@users.noreply.github.com>
2024-08-19 22:48:55 -0700

    Update temp.ts

--- MyTripList/temp.ts
+++ MyTripList/temp.ts
@@ -6,6 +6,73 @@ import { LagoonTables } from '../../constants';
 import { useLagoon } from '../../hooks/useLagoon';
 import { FeatureFlagConfigItem } from '../../LagoonItemInterfaces';
 
+// Create a constant mapping of feature flag keys to their corresponding strings
+const FeatureFlagMapping = {
+    isVisionChipEnabled: 'ENABLE_VISION_CHIP',
+    isAnalyticsTrackBackEnabled: 'ENABLE_ANALYTICS_TRACK_BACK',
+    isAnalyticsV1Enabled: 'ENABLE_ANALYTICS_V1',
+    isTrackScreenLoadEnabled: 'ENABLE_TRACK_SCREEN_LOAD',
+    isConsoleLogsEnabled: 'ENABLE_CONSOLE_LOGS',
+    isCostExperienceNativeEnabled: 'ENABLE_COST_EXPERIENCE_NATIVE',
+    isCountySearchAlertEnabled: 'ENABLE_COUNTY_SEARCH_ALERT',
+    isIdentifyNonTieredProviderLocationsEnabled: 'ENABLE_IDENTIFY_NON_TIERED_PROVIDER_LOCATIONS',
+    isFirstVisitModalEnabled: 'ENABLE_FIRST_VISIT_MODAL',
+    isLowLatencyModeEnabled: 'ENABLE_LLM',
+    isMedicalReferralsActiveEnabled: 'ENABLE_MEDICAL_REFERRALS_ACTIVE',
+    isMockCompareCardEnabled: 'ENABLE_MOCK_COMPARE_CARD',
+    isNewLocationSelectionFlowEnabled: 'ENABLE_NEW_LOCATION_SELECTION_FLOW',
+    isPreferredFacilityIdentificationEnabled: 'ENABLE_PREFERRED_FACILITY_IDENTIFICATION',
+    isShowEapTileEnabled: 'ENABLE_SHOW_EAP_TILE',
+    isUpcomingAvailabilityEnabled: 'ENABLE_UPCOMING_AVAILABILITY',
+    isAcoFilterEnabled: 'ENABLE_ACO_FILTER',
+    isFilterBitesEnabled: 'ENABLE_FILTER_BITES',
+    isShowSelectedLocationAcoProviderEnabled: 'ENABLE_SHOW_SELECTED_LOCATION_ACO_PROVIDER',
+    isShowEthnicityEnabled: 'ENABLE_SHOW_ETHNICITY',
+    isRecentDentalVisitEnabled: 'ENABLE_RECENT_DENTAL_VISIT',
+    isRecentVisionVisitEnabled: 'ENABLE_RECENT_VISION_VISIT',
+    isExtendedTypeaheadAnalyticsEnabled: 'ENABLE_EXTENDED_TYPEAHEAD_ANALYTICS',
+    isPlanningForCareEnabled: 'ENABLE_PLANNING_FOR_CARE',
+    isSuppressBehavioralHealthContentEnabled: 'ENABLE_SUPPRESS_BEHAVIORAL_HEALTH_CONTENT',
+    isPreferredFacilitySearchEnabled: 'ENABLE_PREFERRED_FACILITY_SEARCH',
+    isBehavioralVirtualPsuedocodesEnabled: 'ENABLE_BEHAVIORAL_VIRTUAL_PSUEDOCODES',
+    isPreferredProviderBadgeEnabled: 'ENABLE_PREFERRED_PROVIDER_BADGE',
+    isAnalyticsPcpSelectionFlowEnabled: 'ENABLE_ANALYTICS_PCP_SELECTION_FLOW',
+    isAnalyticsErrorNullSearchPageEnabled: 'ENABLE_ANALYTICS_ERROR_NULL_SEARCH_PAGE',
+    isAnalyticsErrorNullStatePageEnabled: 'ENABLE_ANALYTICS_ERROR_NULL_STATE_PAGE',
+    isAnalyticsMixedResultsPageEnabled: 'ENABLE_ANALYTICS_MIXED_RESULTS_PAGE',
+    isAnalyticsIntroViewOfPsVideoEnabled: 'ENABLE_ANALYTICS_INTRO_VIEW_OF_PSX_VIDEO',
+    isNewPreferredFacilityL2PageEnabled: 'ENABLE_NEW_PREFERRED_FACILITY_L2_PAGE',
+    isAutocompleteCaptureResultsEnabled: 'ENABLE_AUTOCOMPLETE_CAPTURE_RESULTS',
+    isAnalyticsMapViewEnabled: 'ENABLE_ANALYTICS_MAP_VIEW',
+    isAnalyticsFindCareLandingPageEnabled: 'ENABLE_ANALYTICS_FIND_CARE_LANDING_PAGE',
+    isDisplayAvailabilityProviderDetailsEnabled: 'ENABLE_DISPLAY_AVAILABILITY_PROVIDER_DETAILS',
+    isCosmosEnabled: 'ENABLE_COSMOS',
+    isTypeaheadGeneralSearchEnabled: 'ENABLE_TYPEAHEAD_GENERAL_SEARCH',
+    isPsxProviderRecommendationsEnabled: 'ENABLE_PSX_PROVIDER_RECOMMENDATIONS',
+    isOxfordPlnFlagEnabled: 'ENABLE_OXFORD_PLN_FLAG',
+    isAutocompleteRollUpCodeNameSearchEnabled: 'ENABLE_AUTOCOMPLETE_ROLL_UP_CODE_NAME_SEARCH',
+    isPcpTerminationDateEnabled: 'ENABLE_PCP_TERMINATION_DATE',
+    isShowAcceptsMedicaidEnabled: 'ENABLE_SHOW_ACCEPTS_MEDICAID',
+    isAnalyticsListViewPageEnabled: 'ENABLE_ANALYTICS_LIST_VIEW_PAGE',
+    isAdditionalResourcesEnabled: 'ENABLE_ADDITIONAL_RESOURCES',
+    isProgramsDisplayEnabled: 'ENABLE_PROGRAMS_DISPLAY',
+    isPreEffectivePlansEnabled: 'ENABLE_PRE_EFFECTIVE_PLANS',
+    isShowIsPreEffectiveIndicatorEnabled: 'ENABLE_SHOW_IS_PRE_EFFECTIVE_INDICATOR',
+    isFindEligiblePCPBannerEnabled: 'ENABLE_FIND_ELIGIBLE_PCP_BANNER',
+    isLlmEnabled: 'ENABLE_LLM'
+} as const;
+
+// Extract the keys to create a type
+type FeatureFlagKeys = keyof typeof FeatureFlagMapping;
+
+// Create a constant array of keys if needed
+const featureFlagKeys: FeatureFlagKeys[] = Object.keys(FeatureFlagMapping) as FeatureFlagKeys[];
+
+console.log(featureFlagKeys); // Logs all the keys from FeatureFlagMapping
+
+// Example: Access a specific value by key
+console.log(FeatureFlagMapping.isVisionChipEnabled); // Outputs: ENABLE_VISION_CHIP
+
 function createDefaultFeatureFlags<T>(): T {
     const defaultFlags = {} as Record<keyof T, boolean>;
 

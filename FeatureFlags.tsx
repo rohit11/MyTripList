92712eeae0692571c9b1856f1aa1f83a089bb66a
Rohit Nisal <rohit11@users.noreply.github.com>
2024-11-08 18:10:45 -0800

    Update FeatureFlags.tsx

--- FeatureFlags.tsx
+++ FeatureFlags.tsx
@@ -4,5 +4,6 @@ export const FeatureFlagMapping =  {
   isFeaure3Enabled: "ENABLE_FEATURE_3",
   isFeaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Enabled:
     "ENABLE_Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure_3",
+    isFeaure4Enabled: "ENABLE_FEATURE_4",
   
 } as const;

d8d6139e34c5e65afab9a4295a04b27569c57db2
Rohit Nisal <rohit11@users.noreply.github.com>
2024-11-13 23:10:22 -0800

    Update FeatureFlags.tsx

--- FeatureFlags.tsx
+++ FeatureFlags.tsx
@@ -1,8 +1,8 @@
 export enum FeatureFlag {
   'ENABLE_FEATURE_1',
-  'ENABLE_FEATURE_2,
-  'ENABLE_Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure_3,
+  'ENABLE_FEATURE_2',
+  'ENABLE_Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure_3',
   'ENABLE_FEATURE_4',
-  'ENABLE_FEATURE_5,
-  'ENABLE_FEATURE_88,
+  'ENABLE_FEATURE_5',
+  'ENABLE_FEATURE_88',
 }

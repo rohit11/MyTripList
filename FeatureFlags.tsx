20f381d8d0c4d2d6ae9e7f9dfff41a98e773d26b
Rohit Nisal <rohit11@users.noreply.github.com>
2024-11-13 20:05:11 -0800

    Update FeatureFlags.tsx

--- FeatureFlags.tsx
+++ FeatureFlags.tsx
@@ -7,3 +7,11 @@ export const FeatureFlagMapping =  {
     isFeaure5Enabled: "ENABLE_FEATURE_5",
 
 } as const;
+
+export enum FeatureFlag {
+ "ENABLE_FEATURE_1",
+   "ENABLE_FEATURE_2",
+    "ENABLE_Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure3Feaure_3",
+     "ENABLE_FEATURE_4",
+     "ENABLE_FEATURE_5",
+}
